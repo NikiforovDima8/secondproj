3d4326dee559171b2b02f0d1f73429af529d68be
NikiforovDima8 <dimasya88nikiforov@gmal.com>
2023-08-02 08:18:36 +0300

    finish project

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+// const nav = document.querySelector('#nav');
+// const navBtn = document.querySelector('#nav-btn');
+// const navBtnImg = document.querySelector('#nav-btn-img');
+
+// navBtn.onclick = () => {
+//     if (nav.classList.toggle('open')) {
+//         navBtnImg.src ='./img/close.svg';
+//     } else {
+//         navBtnImg.src ='./img/NAV.svg';
+//     }
+// };
+
+
+AOS.init()
+
+
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
 
-navBtn.onclick = () => {
+navBtn.addEventListener('click',()=>{
     if (nav.classList.toggle('open')) {
-        navBtnImg.src ='./img/close.svg';
-    } else {
+     navBtnImg.src ='./img/close.svg';
+    } else{
         navBtnImg.src ='./img/NAV.svg';
     }
-};
+})
+
+if(nav.classList.toggle('open')){
+    nav.style.display=='flex'
+} else{
+    nav.style.display=='none'
+}
+    
